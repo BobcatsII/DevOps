b87a524556dbd1d83b5f88e5a3f834a496097094
BobcatsII <286577399@qq.com>
2018-07-12 18:06:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <286577399@qq.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <28657@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
